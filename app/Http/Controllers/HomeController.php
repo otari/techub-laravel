579263a5c09c3e2f9f16f1fe52fc0048f8a38ca0
Otar Tsilosani <otsilosani@productsavvy.com>
2019-11-13 21:03:02 +0400

    request validations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreUser;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
     	return view('welcome');
+    }
+
+    /**
+     * [register description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function register(StoreUser $request)
+    {
+    	$request->validate(['title' => 'required']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 //Controller Based Route
 Route::get('/', ['uses' => 'HomeController@index'])->middleware('checkAge');
 
+
 //Simple Closure 
 Route::get('/welcome', function () {
     return view('welcome');
